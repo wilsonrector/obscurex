3275919355b72bf2859a4dd48306605577e13ca8
Robert Peterson <robert.peterson@gmail.com>
2008-11-07 15:36:23 -0800

    Rails 2.0.2 to 2.1 Lawl Wilson is silly

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -6,5 +6,10 @@ class ApplicationController < ActionController::Base
 
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
-  protect_from_forgery # :secret => '5efafa4aba7d6caa1cee031b8c1cc252'
+  protect_from_forgery # :secret => '1843ca938021b266176d199f4809f83a'
+  
+  # See ActionController::Base for details 
+  # Uncomment this to filter the contents of submitted sensitive data parameters
+  # from your application log (in this case, all fields with names like "password"). 
+  # filter_parameter_logging :password
 end
