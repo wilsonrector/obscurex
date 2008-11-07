b6e52cf60efb2cb1d0ca2bcf3a8d9b81ae90fdd3
Wilson Rector <wilson@wilsonrector.com>
2008-11-07 15:34:29 -0800

    Added blank Rails Project Nothing to see here, yet.

---
+++ app/controllers/application.rb
@@ -0,0 +1,10 @@
+# Filters added to this controller apply to all controllers in the application.
+# Likewise, all the methods added will be available for all controllers.
+
+class ApplicationController < ActionController::Base
+  helper :all # include all helpers, all the time
+
+  # See ActionController::RequestForgeryProtection for details
+  # Uncomment the :secret if you're not using the cookie session store
+  protect_from_forgery # :secret => '5efafa4aba7d6caa1cee031b8c1cc252'
+end
